517926b6b981c401151662d89bf52aa050c3cf99
Carlos Canales <carlos.canales@estudiante.uc.cl>
2025-08-31 15:56:05 -0400

    semifinal

--- esp32_BT_test/src/main.cpp
+++ esp32_BT_test/src/main.cpp
@@ -1,17 +1,13 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 
-
-
-
 int anillo1_pin = 4;
 int anillo2_pin = 5;
-
 int pins_motores_izquierda[] = {
-  19, 18, //motor1_pin1, motor1_pin2
+  26, 32, //motor1_pin1, motor1_pin2
 };
 int pins_motores_derecha[] = {
-  17, 16, //motor3_pin1, motor3_pin2
+  27, 33, //motor3_pin1, motor3_pin2
 };
 int numero_de_pines = sizeof(pins_motores_izquierda) / sizeof(pins_motores_izquierda[0]);
 
@@ -56,18 +52,15 @@ void avanzar() {
 void retroceder() {
   movimiento_ruedas(pins_motores_izquierda, "retroceder");
   movimiento_ruedas(pins_motores_derecha, "retroceder");
-
 }
 
 void frenar() {
   movimiento_ruedas(pins_motores_izquierda, "detenerse");
   movimiento_ruedas(pins_motores_derecha, "detenerse");
-
 }
 
 void girar(const char* direccion) {
   //Girar con argumento de direccion
-
   if (direccion == "derecha"){
     movimiento_ruedas(pins_motores_izquierda, "avanzar");
     movimiento_ruedas(pins_motores_derecha, "retroceder");
